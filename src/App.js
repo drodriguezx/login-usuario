e1559dd547f26f64964c84d431541682dc37ea03
David Rodriguez <drodriguezx@gmail.com>
2022-08-07 10:55:57 -0300

    modificando login

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import login from './component/login.js';
+
+function App() {
+  return (
+    <div className="App">
+       <login />
+       
+          </div>
+  );
+}
+
+export default login;
